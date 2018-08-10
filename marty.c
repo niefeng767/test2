d29e56d0158d1cc8f367a0901de6b2f7c578ab69
Marty <niefeng767@gmail.com>
2018-08-10 15:50:58 +0800

    add 4 hello

--- marty.c
+++ marty.c
@@ -4,6 +4,10 @@ int main(void)
 	printf("hello\n");
 	printf("hello2\n");
 	printf("hello3\n");
+	printf("hello3\n");
+	printf("hello3\n");
+	printf("hello3\n");
+	printf("hello3\n");
 	return;
 
 }
